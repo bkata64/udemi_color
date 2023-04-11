1ce6334bef3604dd50db8ef2c075bea4f8001e36
bkata64 <64857455+bkata64@users.noreply.github.com>
2023-04-12 00:06:02 +0200

    test and code button click

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, logRoles, render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('button has correct initial color', () => {
+  const { container } = render(<App />);
+  logRoles(container);
+  const colorButton = screen.getByRole('button', {name: 'Change to blue'});
+  expect(colorButton).toHaveStyle({backgroundColor: 'red'})
+});
+
+test('button turns blue when clicked', () => {
   render(<App />);
-  const linkElement = screen.getByText('Learn React');
-  expect(linkElement).toBeInTheDocument();
+  const colorButton = screen.getByRole('button', {name: 'Change to blue'});
+  fireEvent.click(colorButton);
+  expect(colorButton).toHaveStyle({backgroundColor: 'blue'})
+  expect(colorButton).toHaveTextContent('Change to red')
 });
